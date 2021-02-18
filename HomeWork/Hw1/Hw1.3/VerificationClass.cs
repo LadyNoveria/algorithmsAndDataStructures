5aa6745ac195b78acdde6aa4d1a022f799e484e4
Noveria <little.noveria@gmail.com>
2021-02-19 00:43:42 +0300

    Hw1, task 3

---
+++ HomeWork/Hw1/Hw1.3/VerificationClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VerificationLibrary
+{
+    public class VerificationClass
+    {
+        public string numberForCheck { get; set; }
+        public string expectedValue { get; set; }
+        public VerificationClass(string value, string ExpectedValue)
+        {
+            numberForCheck = value;
+            expectedValue = ExpectedValue;
+        }
+        public VerificationClass(string ExpectedValue)
+        {
+            expectedValue = ExpectedValue;
+        }
+    }
+}
